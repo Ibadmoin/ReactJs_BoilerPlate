8492f9dd9ab4757627677848ca9c61514b682ffb
hasnainraza1212 <hr961992@gmail.com>
2023-12-17 17:20:15 +0500

    ok

--- src/Pages/Admin.jsx
+++ src/Pages/Admin.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
-import CustomTable from '../Components/CustomTable'
 import { Avatar, Box, Typography , Button} from '@mui/material';
+import CustomTableMui from '../Components/CustomTableMui';
+import { students } from '../../utils';
+
 const Admin = () => {
   return (
     <Box>
-      <Box className={"flex justify-between items-center"}>
+      <Box className={"flex justify-between items-center m-10"}>
         <Box className={"w-[200px] items-center flex "}>
                 <Avatar alt="Remy Sharp"
                 src="/static/images/avatar/1.jpg"
                 sx={{ 
                     width: 56,
-                    height: 56,
-                    bgcolor: "blue" 
+                    height: 56
                      }}
                  />
                 <Typography sx={{
@@ -22,10 +23,10 @@ const Admin = () => {
                 </Typography>
             </Box>
         <Box className={"w-[200px]flex ml-4 "}>
-             <Button variant="contained">Text</Button>
+             <Button variant="contained">Add Student</Button>
              </Box>
       </Box>
-        <CustomTable/>
+      <CustomTableMui data = {students}/>
     </Box>
   )
 }
